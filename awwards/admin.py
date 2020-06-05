cbf9ce460dcea0df72c32ddab5f61dd0ee915d83
derrickip34 <derrickip34@gmail.com>
2020-06-05 13:46:29 +0300

    register models to admin site

--- awwards/admin.py
+++ awwards/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile,Category,Rating,Project
 
 # Register your models here.
+admin.site.register(Project)
+admin.site.register(Profile)
+admin.site.register(Category)
+admin.site.register(Rating)
