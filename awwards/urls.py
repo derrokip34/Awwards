cfb8e4775d1eb7bbb1a7afd8c4d7b8f65c57484a
derrickip34 <derrickip34@gmail.com>
2020-06-06 12:22:35 +0300

    create a profile page

--- awwards/urls.py
+++ awwards/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$',views.home,name='home')
+    url(r'^$',views.home,name='home'),
+    url(r'^profile/id/(?P<id>\d+)',views.profile,name='profile'),
 ]
--- awwards/views.py
+++ awwards/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from .models import Profile,Project,Category
 
 # Create your views here.
 def home(request):
 
     title = 'Welcome to Awwards'
     return render(request, 'index.html',{'title':title})
+
+def profile(request,id):
+    current_user = request.user
+    user = User.objects.filter(id=id).first()
+    user_profile = user.profile
+
+    title = f'{user.username} profile'
+    return render(request,'profile/profile.html',{'title':title,'current_user':current_user,'user':user,'profile':user_profile})
