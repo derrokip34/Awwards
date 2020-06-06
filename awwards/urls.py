651f1843e71401e2b713c0f7e5fb2fbe85b01d82
derrickip34 <derrickip34@gmail.com>
2020-06-06 13:31:31 +0300

    add default profile image

--- awwards/urls.py
+++ awwards/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$',views.home,name='home'),
     url(r'^profile/id/(?P<id>\d+)',views.profile,name='profile'),
     url(r'^update_profile/',views.update_profile,name='updateProfile'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
