caa8973c63ba3d6a988595e8c724a2e4688684da
derrickip34 <derrickip34@gmail.com>
2020-06-06 12:39:26 +0300

    add an update_profile page

--- awwards/urls.py
+++ awwards/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$',views.home,name='home'),
     url(r'^profile/id/(?P<id>\d+)',views.profile,name='profile'),
+    url(r'^update_profile/',views.update_profile,name='updateProfile'),
 ]
--- awwards/views.py
+++ awwards/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from .models import Profile,Project,Category
+from .forms import UpdateUserForm,UpdateProfileForm
 
 # Create your views here.
 def home(request):
+    current_user = request.user
 
     title = 'Welcome to Awwards'
     return render(request, 'index.html',{'title':title})
@@ -15,3 +17,20 @@ def profile(request,id):
 
     title = f'{user.username} profile'
     return render(request,'profile/profile.html',{'title':title,'current_user':current_user,'user':user,'profile':user_profile})
+
+def update_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST,instance=request.user)
+        profile_form = UpdateProfileForm(request.POST,request.FILES,instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            return redirect('profile',id=current_user.id)
+
+    else:
+        user_form = UpdateUserForm(instance=request.user)
+        profile_form = UpdateProfileForm(instance=request.user.profile)
+    
+    title = f'Update {current_user.username} profile'
+    return render(request,'profile/update_profile.html',{'title':title,'current_user':current_user,'u_form':user_form,'p_form':profile_form})
