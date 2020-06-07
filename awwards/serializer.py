db643b0464c1dd00b39f4314e2962e3c4951a59e
derrickip34 <derrickip34@gmail.com>
2020-06-07 15:28:22 +0300

    create projectserializer class

--- awwards/serializer.py
+++ awwards/serializer.py
@@ -5,3 +5,8 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ('__all__')
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ('__all__')
